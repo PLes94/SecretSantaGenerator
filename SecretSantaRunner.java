a5c3ba505d29922c9dc9f2867e9af93c048afda5
Piotr L <suspicioushero@gmail.com>
2022-11-29 12:25:05 +0100

    Update SecretSantaRunner to avoid duplicates, add gitignore file

--- SecretSantaRunner.java
+++ SecretSantaRunner.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 
 public class SecretSantaRunner {
-    public static void main(String args[]) {
-      List<String> lista = Arrays.asList("Monika", "Ola", "Paweł", "Bogusia", "Ania", "Szymon", "Bartek", "Natka", "Piotrek");
-      
-      System.out.println(lista);
-      
-      Collections.shuffle(lista);
-      
-      System.out.println(lista);
+    public static void main(String[] args) {
+//      List<String> list = Arrays.asList("Monika", "Ola", "Paweł", "Bogusia", "Ania", "Szymon", "Bartek", "Natka", "Piotrek");
+        List<String> list = Arrays.asList("Monika", "Ola", "Paweł", "Piotr", "Marek", "Patrycja", "Beata", "Marcin", "Łukasz", "Irenka");
+
+        System.out.println(list);
+
+        List<String> newList = new ArrayList<>(list);
+        Collections.shuffle(newList);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(newList.get(i))) {
+                Collections.shuffle(newList);
+                i = 0;
+            }
+        }
+
+        System.out.println(newList);
     }
 }
