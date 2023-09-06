31d749c8af6b508c8e1b6c44b6064167a6bc32f5
Piotr Leśniewski <pls@spyro-soft.com>
2023-09-06 15:15:00 +0200

    Add possibility of entering a custom name list

--- com/ples94/secretsanta/SecretSantaRunner.java
+++ com/ples94/secretsanta/SecretSantaRunner.java
@@ -1,23 +1,22 @@
 package com.ples94.secretsanta;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class SecretSantaRunner {
     public static void main(String[] args) {
-//      List<String> list = Arrays.asList("Monika", "Ola", "Paweł", "Bogusia", "Ania", "Szymon", "Bartek", "Natka", "Piotrek");
-        List<String> list = Arrays.asList("Monika", "Ola", "Paweł", "Piotr", "Marek", "Patrycja", "Beata", "Marcin", "Łukasz", "Irenka");
-
-        List<String> newList = new ArrayList<>(list);
-        Collections.shuffle(newList);
+        System.out.println("Enter the list of people participating in the draw separated by a space:");
+        Scanner input = new Scanner(System.in);
+        List<String> initialList = List.of(input.nextLine().split(" "));
+        List<String> shuffledList = new ArrayList<>(initialList);
+        Collections.shuffle(shuffledList);
 
         int i = 0;
-        while (i < list.size()) {
-            if (list.get(i).equals(newList.get(i))) {
-                Collections.shuffle(newList);
+        while (i < initialList.size()) {
+            if (initialList.get(i).equals(shuffledList.get(i))) {
+                Collections.shuffle(shuffledList);
                 i = 0;
             } else {
                 i++;
@@ -25,9 +24,8 @@ public class SecretSantaRunner {
         }
 
         // return list of respective donors and recipients
-        for (i = 0; i < list.size(); i++) {
-            String verbType = list.get(i).endsWith("a") ? " wylosowała" : " wylosował";
-            System.out.println(list.get(i) + verbType + " osobę: " + newList.get(i));
+        for (i = 0; i < initialList.size(); i++) {
+            System.out.println(initialList.get(i) + " drew: " + shuffledList.get(i));
         }
     }
 }
